cad5c49895c473e7b3dffc013c966f0262f7e9d5
savaged-22 <juanpabloespinosa2014@gmail.com>
2024-09-26 10:06:54 -0500

    Corregir error

--- src/main/java/co/musicshop/fis/repositories/SongRepository.java
+++
@@ -1,7 +0,0 @@
-package co.musicshop.fis.repositories;
-
-import co.musicshop.fis.models.Song;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface SongRepository extends JpaRepository<Song, Long> {
-}
